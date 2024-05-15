f6c96495f4f49a8eb975ab356515526d59a7fbab
f1rstf1y9 <f1rstf1y9@naver.com>
2024-05-15 22:58:32 +0900

    fix: button width height

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({ children, onClick, width, height }) {
   return (
     <button
       onClick={onClick}
-      className={`bg-black hover:bg-[#414040] text-white text-sm sm:text-lg rounded ${width ? `w-[${width}]` : 'w-full'} ${height ? `h-[${height}]` : 'h-[52px]'}`}
+      className={`bg-black hover:bg-[#414040] text-white text-sm sm:text-lg rounded ${width ? `${width}` : 'w-full'} ${height ? `${height}` : 'h-[52px]'}`}
     >
       {children}
     </button>
