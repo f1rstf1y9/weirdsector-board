1e8e8c83153ab49d859471f96ad89eb0faf4a0d3
f1rstf1y9 <f1rstf1y9@naver.com>
2024-05-15 23:40:22 +0900

    feat: create checkbox component

---
+++ src/components/CheckBox.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { Checkbox } from '@headlessui/react';
+
+export default function CheckBox() {
+  const [enabled, setEnabled] = useState(false);
+
+  return (
+    <Checkbox
+      checked={enabled}
+      onChange={setEnabled}
+      className='group block w-6 h-6 rounded-[5px] border bg-black bg-[#F9F9F9] border-[#E1E1E1] data-[checked]:bg-black data-[checked]:border-none flex justify-center items-center'
+    >
+      <svg
+        className='stroke-white opacity-0 group-data-[checked]:opacity-100'
+        xmlns='http://www.w3.org/2000/svg'
+        width='12'
+        height='9'
+        viewBox='0 0 12 9'
+        fill='none'
+      >
+        <path
+          d='M10.334 0.999349L3.66731 7.66602L1.00065 4.99935'
+          stroke='white'
+          stroke-width='2'
+          stroke-linecap='round'
+          stroke-linejoin='round'
+        />
+      </svg>
+    </Checkbox>
+  );
+}
