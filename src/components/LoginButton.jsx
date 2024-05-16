ba24ac12d200f24f714b68c14b11eea51dcb9fcc
f1rstf1y9 <f1rstf1y9@naver.com>
2024-05-16 22:27:24 +0900

    fix: class of LoginButton

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 export default function LoginButton() {
   return (
     <>
-      <Link to='login' class='px-10 py-3 border border-[#E1E1E1] rounded'>
-        <span class='text-lg text-[#272727]'>로그인</span>
+      <Link to='login' className='px-10 py-3 border border-[#E1E1E1] rounded'>
+        <span className='text-lg text-[#272727]'>로그인</span>
       </Link>
     </>
   );
