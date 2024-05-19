8e2b2ae48b98a91f71efde0fcb3421b8f0194178
f1rstf1y9 <f1rstf1y9@naver.com>
2024-05-19 22:16:46 +0900

    feat: fetch comments data

--- src/pages/Post/PostPage.jsx
+++ src/pages/Post/PostPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import useBoardValidation from '@hook/useBoardValidation';
 import { useAuthStore } from '@store/store.js';
@@ -17,6 +17,8 @@ function PostPage() {
   const [postObj, setPostObj] = useState(null);
   const [hashtags, setHashtags] = useState([]);
   const [attachmentUrl, setAttachmentUrl] = useState('');
+  const [comment, setComment] = useState('');
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     let isMounted = true;
@@ -91,11 +93,31 @@ function PostPage() {
     };
 
     fetchPostData();
-
     return () => {
       isMounted = false;
     };
   }, [postId, navigate]);
+
+  const fetchCommentsData = async () => {
+    if (postObj) {
+      try {
+        const { data: commentData, error: commentError } = await supabase
+          .from('comments')
+          .select('*')
+          .eq('post_id', postObj.post_id);
+        if (commentError) {
+          throw commentError;
+        }
+        setComments(commentData);
+      } catch (error) {
+        console.error('Failed to fetch comments data:', error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchCommentsData();
+  }, [postObj]);
 
   const formatTimestamp = (timestamp) => {
     const date = parseISO(timestamp);
@@ -134,7 +156,7 @@ function PostPage() {
               <div>
                 {user?.id === postObj?.user_id && (
                   <Dropdown
-                    anchor='bottom start'
+                    anchor='bottom end'
                     items={[
                       {
                         content: '수정',
@@ -204,52 +226,64 @@ function PostPage() {
           )}
 
           {user && (
-            <div className='w-100 flex gap-[20px] '>
+            <form className='w-100 flex gap-[20px] '>
               <input
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
                 required
                 className='min-w-0 h-[55px] flex-1 shrink bg-white rounded border border-[#E1E1E1] focus:border-black focus:!border-black px-5 py-3.5 flex gap-4'
               ></input>
               <Button width='shrink-0 w-[88px] sm:w-[142px]'>댓글 작성</Button>
-            </div>
+            </form>
           )}
 
-          <hr />
-          <div className='flex flex-col gap-[16px]'>
-            <div className='flex items-center justify-between'>
-              <p className='text-lg sm:text-xl font-bold'>나는 호빵맨</p>
-              <Dropdown
-                anchor='bottom start'
-                items={[
-                  {
-                    content: '수정',
-                    onClick: () => {},
-                  },
-                  {
-                    content: '삭제',
-                    onClick: () => {},
-                  },
-                ]}
-              >
-                <svg
-                  width='4'
-                  className='h-[18px] sm:h-[24px]'
-                  viewBox='0 0 4 26'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path
-                    d='M0 2.96863C0 3.49906 0.210713 4.00777 0.585786 4.38284C0.960859 4.75791 1.46957 4.96863 2 4.96863C2.53043 4.96863 3.03914 4.75791 3.41421 4.38284C3.78929 4.00777 4 3.49906 4 2.96863C4 2.4382 3.78929 1.92949 3.41421 1.55441C3.03914 1.17934 2.53043 0.968628 2 0.968628C1.46957 0.968628 0.960859 1.17934 0.585786 1.55441C0.210713 1.92949 0 2.4382 0 2.96863ZM0 12.9686C0 13.4991 0.210713 14.0078 0.585786 14.3828C0.960859 14.7579 1.46957 14.9686 2 14.9686C2.53043 14.9686 3.03914 14.7579 3.41421 14.3828C3.78929 14.0078 4 13.4991 4 12.9686C4 12.4382 3.78929 11.9295 3.41421 11.5544C3.03914 11.1793 2.53043 10.9686 2 10.9686C1.46957 10.9686 0.960859 11.1793 0.585786 11.5544C0.210713 11.9295 0 12.4382 0 12.9686V12.9686ZM0 22.9686C0 23.4991 0.210713 24.0078 0.585786 24.3828C0.960859 24.7579 1.46957 24.9686 2 24.9686C2.53043 24.9686 3.03914 24.7579 3.41421 24.3828C3.78929 24.0078 4 23.4991 4 22.9686C4 22.4382 3.78929 21.9295 3.41421 21.5544C3.03914 21.1793 2.53043 20.9686 2 20.9686C1.46957 20.9686 0.960859 21.1793 0.585786 21.5544C0.210713 21.9295 0 22.4382 0 22.9686V22.9686Z'
-                    fill='black'
-                  />
-                </svg>
-              </Dropdown>
-            </div>
+          {comments.map((comment) => (
+            <React.Fragment key={comment.comment_id}>
+              <hr />
+              <div className='flex flex-col gap-[16px]'>
+                <div className='flex items-center justify-between'>
+                  <p className='text-lg sm:text-xl font-bold'>
+                    {comment.user_nickname}
+                  </p>
+                  <div>
+                    {comment.user_id === user.id && (
+                      <Dropdown
+                        anchor='bottom end'
+                        items={[
+                          {
+                            content: '수정',
+                            onClick: () => {},
+                          },
+                          {
+                            content: '삭제',
+                            onClick: () => {},
+                          },
+                        ]}
+                      >
+                        <svg
+                          width='4'
+                          className='h-[18px] sm:h-[24px]'
+                          viewBox='0 0 4 26'
+                          fill='none'
+                          xmlns='http://www.w3.org/2000/svg'
+                        >
+                          <path
+                            d='M0 2.96863C0 3.49906 0.210713 4.00777 0.585786 4.38284C0.960859 4.75791 1.46957 4.96863 2 4.96863C2.53043 4.96863 3.03914 4.75791 3.41421 4.38284C3.78929 4.00777 4 3.49906 4 2.96863C4 2.4382 3.78929 1.92949 3.41421 1.55441C3.03914 1.17934 2.53043 0.968628 2 0.968628C1.46957 0.968628 0.960859 1.17934 0.585786 1.55441C0.210713 1.92949 0 2.4382 0 2.96863ZM0 12.9686C0 13.4991 0.210713 14.0078 0.585786 14.3828C0.960859 14.7579 1.46957 14.9686 2 14.9686C2.53043 14.9686 3.03914 14.7579 3.41421 14.3828C3.78929 14.0078 4 13.4991 4 12.9686C4 12.4382 3.78929 11.9295 3.41421 11.5544C3.03914 11.1793 2.53043 10.9686 2 10.9686C1.46957 10.9686 0.960859 11.1793 0.585786 11.5544C0.210713 11.9295 0 12.4382 0 12.9686V12.9686ZM0 22.9686C0 23.4991 0.210713 24.0078 0.585786 24.3828C0.960859 24.7579 1.46957 24.9686 2 24.9686C2.53043 24.9686 3.03914 24.7579 3.41421 24.3828C3.78929 24.0078 4 23.4991 4 22.9686C4 22.4382 3.78929 21.9295 3.41421 21.5544C3.03914 21.1793 2.53043 20.9686 2 20.9686C1.46957 20.9686 0.960859 21.1793 0.585786 21.5544C0.210713 21.9295 0 22.4382 0 22.9686V22.9686Z'
+                            fill='black'
+                          />
+                        </svg>
+                      </Dropdown>
+                    )}
+                  </div>
+                </div>
 
-            <p>댓글</p>
-            <p className='text-sm sm:text-base text-[#B8B7B7]'>
-              2023.07.19 11:53:50
-            </p>
-          </div>
+                <p>{comment.content}</p>
+                <p className='text-sm sm:text-base text-[#B8B7B7]'>
+                  {formatTimestamp(comment.created_at)}
+                </p>
+              </div>
+            </React.Fragment>
+          ))}
         </div>
       </div>
     </>
