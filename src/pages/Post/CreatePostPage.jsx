f1636081a4da475a9d51aca7c2127bef67d6740a
f1rstf1y9 <f1rstf1y9@naver.com>
2024-05-21 17:36:53 +0900

    fix: svg pascal-case to camelCase

--- src/pages/Post/CreatePostPage.jsx
+++ src/pages/Post/CreatePostPage.jsx
@@ -172,7 +172,7 @@ function CreatePostPage() {
                       xmlns='http://www.w3.org/2000/svg'
                       style={{ pointerEvents: 'none' }}
                     >
-                      <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+                      <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
                       <g
                         id='SVGRepo_tracerCarrier'
                         strokeLinecap='round'
--- src/pages/Post/UpdatePostPage.jsx
+++ src/pages/Post/UpdatePostPage.jsx
@@ -236,7 +236,7 @@ function UpdatePostPage() {
                       xmlns='http://www.w3.org/2000/svg'
                       style={{ pointerEvents: 'none' }}
                     >
-                      <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+                      <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
                       <g
                         id='SVGRepo_tracerCarrier'
                         strokeLinecap='round'
